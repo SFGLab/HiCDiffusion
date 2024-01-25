a795acd46889f56f98f2961c3f3c5029b30e39bd
Mateusz Chilinski <mateusz@chilinski.eu>
2024-01-25 16:19:24 +0100

    automatic detection "chr" in name of chromosomes

--- hic_dataset.py
+++ hic_dataset.py
@@ -7,6 +7,12 @@ import cooler
 class HiCDataset():
     def __init__(self, hic_file_name, res):
         self.hic_dataset = cooler.Cooler(f"{hic_file_name}::/resolutions/{res}")
+        if("chr" in self.hic_dataset.chromnames[0]):
+            self.remove_chr = False
+        else:
+            self.remove_chr = True
 
     def get(self, chromosome, start, window = 2097152, res = 10000):
+        if(self.remove_chr):
+            chromosome = chromosome.replace("chr", "")
         return np.log(self.hic_dataset.matrix(field="count", balance=None).fetch(f"{chromosome}:{start}-{start+window}")+1)
